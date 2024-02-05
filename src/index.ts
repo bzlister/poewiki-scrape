735b4564ff034dac7d12165a3109bf7d2d7f1783
bzlis <bzlis@github.com>
2024-02-05 16:58:26 -0500

    item mods

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,28 @@ import { parse, stringify } from "yaml";
 type Config = {
   out: string;
   nodes: string[];
-  items: string[];
+  items: (string | Record<string, string[]>)[];
+  skills: string[];
 };
 
 const config: Config = parse(fs.readFileSync("./config.yml", "utf8"));
 if (
   config.out.charAt(config.out.length - 1) !== "/" ||
   config.out.charAt(config.out.length - 1) !== "\\"
-)
+) {
   config.out = config.out + "/";
+}
 
 const assets = {
   nodes: collateAssets(fs.readdirSync("assets/nodes")),
   items: collateAssets(fs.readdirSync("assets/items")),
+  skills: collateAssets(fs.readdirSync("assets/skills")),
 };
-
-console.log(JSON.stringify(assets));
 
 Promise.all([
   ...(config.nodes || []).map(getNodeAsset),
   ...(config.items || []).map(getItemAsset),
+  ...(config.skills || []).map(getSkillAsset),
 ]).then(() => console.log("Done"));
 
 function collateAssets(filenames: string[]): Record<string, string> {
@@ -51,6 +53,7 @@ function getNodeAsset(name: string): Promise<void> {
       return captureWebsite
         .file(`https://www.poewiki.net/wiki/${encoded}`, file, {
           element: "div.info-card__card",
+          overwrite: true,
         })
         .then(() => fs.promises.copyFile(file, `assets/nodes/${name}.png`));
     }
@@ -60,7 +63,9 @@ function getNodeAsset(name: string): Promise<void> {
   }
 }
 
-function getItemAsset(name: string): Promise<void> {
+function getItemAsset(item: string | Record<string, string[]>): Promise<void> {
+  const name = typeof item === "string" ? item : Object.keys(item)[0];
+
   try {
     if (assets.items[name]) {
       return fs.promises.copyFile(
@@ -70,14 +75,58 @@ function getItemAsset(name: string): Promise<void> {
     } else {
       const encoded = escape(name.replaceAll(" ", "_"));
       const file = `${config.out}${name}.png`;
+      let scripts: string | undefined;
+      if (typeof item !== "string") {
+        scripts = `
+const root = document.querySelector("span.item-box");
+const table = root.querySelector("table");
+if (table) {
+  table.remove();
+}
+const mods = root.querySelector("span.item-stats > span.group.tc.-mod");
+`;
+
+        const createTextNodes = item[name].map(
+          (mod) => `mods.appendChild(document.createTextNode("${mod}"));`
+        );
+        scripts += createTextNodes.reduce(
+          (prev, curr) =>
+            `${prev}\r\nmods.appendChild(document.createElement("br"));\r\n${curr}`
+        );
+      }
       return captureWebsite
         .file(`https://www.poewiki.net/wiki/${encoded}`, file, {
           element: "span.item-box",
+          overwrite: true,
+          ...(scripts && { modules: [scripts] }),
         })
         .then(() => fs.promises.copyFile(file, `assets/items/${name}.png`));
     }
   } catch (err) {
     console.log(`Error getting item asset ${name}.\nError: ${err}`);
+    return Promise.resolve();
+  }
+}
+
+export function getSkillAsset(name: string): Promise<void> {
+  try {
+    if (assets.skills[name]) {
+      return fs.promises.copyFile(
+        `assets/skills/${assets.skills[name]}`,
+        `${config.out}${assets.skills[name]}`
+      );
+    } else {
+      const encoded = escape(name.replaceAll(" ", "_"));
+      const file = `${config.out}${name}.png`;
+      return captureWebsite
+        .file(`https://www.poewiki.net/wiki/${encoded}`, file, {
+          element: "span.item-box",
+          overwrite: true,
+        })
+        .then(() => fs.promises.copyFile(file, `assets/skills/${name}.png`));
+    }
+  } catch (err) {
+    console.log(`Error getting skill asset ${name}.\nError: ${err}`);
     return Promise.resolve();
   }
 }
